4a947e89fc9794138d359c4fdc241405261a9024
Roshan Suntharan <roshan.suntharan@mail.utoronto.ca>
2020-01-12 18:43:29 -0500

    Re-did as a project

---
+++ Homework1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Input your first name: ");
+			string firstName = Console.ReadLine();
+			Console.WriteLine("Input your surname: ");
+			string lastName = Console.ReadLine();
+			Console.WriteLine("Input your age: ");
+			int age = Int32.Parse(Console.ReadLine());
+			Console.WriteLine("Input your weight (in kg): ");
+			float weight = (float)Double.Parse(Console.ReadLine());
+			Console.WriteLine("Input your height (in cm): ");
+			float height = (float)Double.Parse(Console.ReadLine());
+
+			Console.WriteLine(firstName + lastName + " is " + age + " years old, his weight is "
+				+ weight + " kg and his height is " + height + " cm.");
+
+			float bodyMassIndex = weight / ((height / 100) * (height / 100));
+			Console.WriteLine("The BMI of " + firstName + lastName + " is " + bodyMassIndex);
+		}
+    }
+}

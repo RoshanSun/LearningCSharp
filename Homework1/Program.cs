f8b23a6e41a6e51b8871e9c68df603fdf4698782
Roshan Suntharan <roshan.suntharan@mail.utoronto.ca>
2020-01-12 18:49:51 -0500

    Allow code for multiple runs

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -6,22 +6,30 @@ namespace Homework1
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Input your first name: ");
-			string firstName = Console.ReadLine();
-			Console.WriteLine("Input your surname: ");
-			string lastName = Console.ReadLine();
-			Console.WriteLine("Input your age: ");
-			int age = Int32.Parse(Console.ReadLine());
-			Console.WriteLine("Input your weight (in kg): ");
-			float weight = (float)Double.Parse(Console.ReadLine());
-			Console.WriteLine("Input your height (in cm): ");
-			float height = (float)Double.Parse(Console.ReadLine());
+			String response;
 
-			Console.WriteLine(firstName + lastName + " is " + age + " years old, his weight is "
-				+ weight + " kg and his height is " + height + " cm.");
+			do
+			{
+				Console.WriteLine("Input your first name: ");
+				string firstName = Console.ReadLine();
+				Console.WriteLine("Input your surname: ");
+				string lastName = Console.ReadLine();
+				Console.WriteLine("Input your age: ");
+				int age = Int32.Parse(Console.ReadLine());
+				Console.WriteLine("Input your weight (in kg): ");
+				float weight = (float)Double.Parse(Console.ReadLine());
+				Console.WriteLine("Input your height (in cm): ");
+				float height = (float)Double.Parse(Console.ReadLine());
 
-			float bodyMassIndex = weight / ((height / 100) * (height / 100));
-			Console.WriteLine("The BMI of " + firstName + lastName + " is " + bodyMassIndex);
+				Console.WriteLine(firstName + lastName + " is " + age + " years old, his weight is "
+					+ weight + " kg and his height is " + height + " cm.");
+
+				float bodyMassIndex = weight / ((height / 100) * (height / 100));
+				Console.WriteLine("The BMI of " + firstName + lastName + " is " + bodyMassIndex);
+
+                Console.WriteLine("Would you like to try again, for a different person?");
+				response = Console.ReadLine();
+			} while (response = "Y");
 		}
     }
 }
